62cec9391d7f73cbe814e841ea71b57f3d2aa70c
Thanh Duy <duythanh19111999@gmail.com>
2024-02-06 01:30:45 +0700

    First commit

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hế lô vợ iu ! 🖐🖐🖐',
+    introDesc: `Anh có điều này muốn nói với em, em nghe anh nói nha`,
+    btnIntro: 'Dạ anh iu',
+    title: 'Em có chương anh nhiều nhắm hokkkk 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là em hết thương anh rồi đó nha :< ',
+    btnYes: 'Dạ có, em chương anh nhìu nhắm 💖💖💖',
+    btnNo: 'Hết thương rồi, cưới về để bào thôi ! 💔 ',
+    question:'Nếu có thì em nói lý do cho anh nghe ikkk 😘😊',
+    reasonPlaceholder: 'Lý do nè',
+    btnReply: 'Gửiiii ! 💌💌',
+    reply: 'Tại vì anh vừa tinh tế, vừa tử tế lại vừa kinh tế 😘😊',
+    mess: 'Hê, kinh tế thì khum dám nhận nhưng sẽ cố gắng có thiệt nhìu để lo cho em 🥰. Anh cũng thương em nhiều lắm 💖💖💖. Happy Anniversary 4th Years || Love you 300.000 😘😘',
+    messDesc: 'P/s: À mà thương thì mua PS5 cho anh chơi nha, hí hí',
+    btnAccept: 'Dạ chằm iu 💖',
+    messLink: 'https://haloshop.vn/may-ps5/ps5-slim-digital-korea-bh-3-thang'
+}
